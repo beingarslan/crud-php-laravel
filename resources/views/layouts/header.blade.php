cbac805a579f59f21d635b507f756b0374af9fc1
ARSLAN ASLAM <beingfastian@gmail.com>
2022-04-30 23:28:25 +0500

    changes

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,30 @@
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="{{ asset('assets/style.css') }}" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="#">AusWine</a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-toggle="modal" data-target="#form">Registation</a
+          >
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="wines.php">Wines</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Details.html">Details</a>
+            </li>
+        </ul>
+    </nav>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/home');
 });
-
+Route::get('/creat/wine', function () {
+    return view('pages/create-wine');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
